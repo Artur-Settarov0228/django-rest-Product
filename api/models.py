4f59fecb8ab426025a3a36886614bafa7c7eb0b3
Artur-Settarov0228 <settarovartur2@gmail.com>
2025-11-29 21:00:26 +0500

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10 , decimal_places=3)
+    stock = models.IntegerField()
+    active = models.BooleanField(default=True)
+    category = models.CharField(max_length=150)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-created_at']
