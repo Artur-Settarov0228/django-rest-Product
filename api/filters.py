f4ec15af546d059a0acb17bffbc333ffbbac0fa3
Artur-Settarov0228 <settarovartur2@gmail.com>
2025-11-29 23:23:28 +0500

    done

--- api/filters.py
+++ api/filters.py
@@ -1,2 +1,14 @@
-import django_filters
+import django_filters 
 from .models import Product
+
+class ProductFilters(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
+    stock = django_filters.NumberFilter(field_name='stock', lookup_expr='gte')
+
+    created_at = django_filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Product
+        fields = ['min_price', 'max_price', 'stock', 'created_at']
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Product
+from .serializers import ProductSerializer
+from .filters import ProductFilters
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_class = ProductFilters
+
+    search_fields = ['name', 'category']
+
+    ordering_fields = ['price', 'stock', 'created_at']
